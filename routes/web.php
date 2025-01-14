c719b6fe96957de3fcd0dbce7241d1ea4194dc8d
[Ahmad Nabil] <nabilrizalibrahim@gmail.com>
2025-01-14 13:49:48 +0800

    web update

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-Route::get('/grants/{grant}', [GrantController::class, 'show'])->name('grants.show');
 
 Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
+/*Route::middleware(['auth'])->get('/dashboard', function () {
+    return view('dashboard');
+});*/
+
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('academicians', AcademicianController::class);
 Route::resource('grants', GrantController::class);
@@ -35,3 +38,14 @@ Route::middleware(['can:AdminAcademicianStaff'])->group(function () {
     Route::resource('grants', GrantController::class);
     Route::resource('milestones', MilestoneController::class);
 });
+
+Route::get('/grants/{grant}', [GrantController::class, 'show'])->name('grants.show');
+
+/*Route::middleware(['auth'])->group(function () {
+    Route::get('/grants/{grant}', [GrantController::class, 'show'])
+        ->middleware('can:isMember,grant');
+
+    Route::get('/grants/{grant}/edit', [GrantController::class, 'edit'])
+        ->middleware('can:isLeader,grant');
+
+});*/
